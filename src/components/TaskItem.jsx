089e6bd0266283e3ba4e1e857f612e935173e33d
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-07-18 17:22:33 -0300

    feat: add function to change a task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -19,6 +19,20 @@ const TaskItem = ({ task, fetchTasks }) => {
         }
     };
 
+    const handleTaskCompletionChange = async (e) => {
+        try {
+            await axios.patch(`http://localhost:8000/tasks/${task._id}`, {
+                isCompleted: e.target.checked,
+            });
+
+            await fetchTasks();
+
+            alert.success("A tarefa foi modificada com sucesso!");
+        } catch (error) {
+            alert.error("Algo deu errado.");
+        }
+    };
+
     return (
         <div className="task-item-container">
             <div className="task-description">
@@ -30,7 +44,11 @@ const TaskItem = ({ task, fetchTasks }) => {
                     }
                 >
                     {task.description}
-                    <input type="checkbox" defaultChecked={task.isCompleted} />
+                    <input
+                        type="checkbox"
+                        defaultChecked={task.isCompleted}
+                        onChange={(e) => handleTaskCompletionChange(e)}
+                    />
                     <span
                         className={
                             task.isCompleted
@@ -40,6 +58,7 @@ const TaskItem = ({ task, fetchTasks }) => {
                     ></span>
                 </label>
             </div>
+
             <div className="delete">
                 <AiFillDelete
                     size={18}
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -12,6 +12,7 @@ const Tasks = () => {
     const fetchTasks = async () => {
         try {
             const { data } = await axios.get("http://localhost:8000/tasks");
+
             setTasks(data);
         } catch (error) {
             console.log(error);
@@ -24,10 +25,10 @@ const Tasks = () => {
 
     return (
         <div className="tasks-container">
-            <h2>Minhas tarefas</h2>
+            <h2>Minhas Tarefas</h2>
 
             <div className="last-tasks">
-                <h3>Ultimas Tarefas</h3>
+                <h3>Últimas Tarefas</h3>
                 <AddTask fetchTasks={fetchTasks} />
                 <div className="tasks-list">
                     {tasks
@@ -39,10 +40,10 @@ const Tasks = () => {
             </div>
 
             <div className="completed-tasks">
-                <h3>Tarefas Concluidas</h3>
+                <h3>Tarefas Concluídas</h3>
                 <div className="tasks-list">
                     {tasks
-                        .filter((task) => task.isCompleted === true)
+                        .filter((task) => task.isCompleted)
                         .map((completedTask) => (
                             <TaskItem
                                 task={completedTask}
