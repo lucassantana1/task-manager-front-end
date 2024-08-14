a33ae740b13ddc8ab61a28d134ab5f3eb41fc4da
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-08-14 16:26:24 -0300

    fix: update dependencies

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
-
 import App from "./App";
 
 const options = {
-    position: positions.BOTTOM_CENTER,
-    timeout: 2500,
-    offset: "30px",
-    transition: transitions.SCALE,
+  position: positions.BOTTOM_CENTER,
+  timeout: 2500,
+  offset: "30px",
+  transition: transitions.SCALE,
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AlertProvider template={AlertTemplate} {...options}>
-                <App />
-            </AlertProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
