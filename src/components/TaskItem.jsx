a2130923fc54f6abcd281a650d3ae19b291556c1
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-08-16 17:55:22 -0300

    feat: change api url to environment variable

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -9,7 +9,7 @@ const TaskItem = ({ task, fetchTasks }) => {
 
   const handleTaskDeletion = async () => {
     try {
-      await axios.delete(`http://localhost:8000/tasks/${task._id}`)
+      await axios.delete(`${process.env.REACT_APP_API_URL}/tasks/${task._id}`)
 
       await fetchTasks()
 
@@ -21,7 +21,7 @@ const TaskItem = ({ task, fetchTasks }) => {
 
   const handleTaskCompletionChange = async (e) => {
     try {
-      await axios.patch(`http://localhost:8000/tasks/${task._id}`, {
+      await axios.patch(`${process.env.REACT_APP_API_URL}/tasks/${task._id}`, {
         isCompleted: e.target.checked,
       })
 
